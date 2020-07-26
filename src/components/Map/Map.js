20934fdcf45dcac516e65d1f487932e444494782
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-07-25 22:21:34 -0400

    add proper markers and popup to map

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,36 +1,71 @@
 import React from 'react';
 import { useState } from 'react';
-import ReactMapGL, { Marker } from 'react-map-gl';
+import ReactMapGL, { Marker, NavigationControl, Popup } from 'react-map-gl';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons';
+import { StyledMarker, StyledPopup } from './Map.styled';
 import locations from '../../data';
 
 function Map() {
   const [viewport, setViewport] = useState({
     latitude: 43.653226,
     longitude: -79.3831843,
-    zoom: 8,
+    zoom: 12,
   });
+
+  const [selectedLocation, setSelectedLocation] = useState(null);
 
   return (
     <ReactMapGL
       {...viewport}
       width='100vw'
-      height='100vh'
+      height='92vh'
       onViewportChange={nextViewport => setViewport(nextViewport)}
       mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       mapStyle='mapbox://styles/mapbox/streets-v11'
       style={{ position: 'relative' }}
     >
+      <div
+        style={{
+          position: 'absolute',
+          right: '10px',
+          bottom: '10px',
+        }}
+      >
+        <NavigationControl showCompass={false} />
+      </div>
       {locations.map(location => (
         <Marker
-          key={location.phone}
+          key={location.address}
           latitude={location.lat}
           longitude={location.lng}
         >
-          <button>
-            <img src='../../canada.svg' alt='Marker' />
-          </button>
+          <StyledMarker
+            onClick={e => {
+              e.preventDefault();
+              setSelectedLocation(location);
+            }}
+          >
+            <FontAwesomeIcon icon={faMapMarker} />
+          </StyledMarker>
         </Marker>
       ))}
+      {selectedLocation && (
+        <Popup
+          latitude={selectedLocation.lat}
+          longitude={selectedLocation.lng}
+          onClose={() => {
+            setSelectedLocation(null);
+          }}
+        >
+          <StyledPopup>
+            <h3>{selectedLocation.agency}</h3>
+            <h4>{selectedLocation.program}</h4>
+            <p>{selectedLocation.address}</p>
+            <p>{selectedLocation.phone}</p>
+          </StyledPopup>
+        </Popup>
+      )}
     </ReactMapGL>
   );
 }
---
+++ src/components/Map/Map.styled.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components';
+
+export const StyledMarker = styled.button`
+  border: none;
+  background: none;
+  cursor: pointer;
+
+  & svg {
+    color: ${({ theme }) => theme.red};
+  }
+`;
+
+export const StyledPopup = styled.div`
+  width: 300px;
+
+  & h3 {
+    font-size: 16px;
+  }
+
+  & h4 {
+    font-size: 14px;
+  }
+
+  & p {
+    font-size: 14px;
+  }
+`;
