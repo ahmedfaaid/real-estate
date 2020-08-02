e7f9d0a4167944090728c1410721e2784cd1f0d7
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-08-02 00:41:35 -0400

    add apollo client

--- src/index.js
+++ src/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import { ThemeProvider } from 'styled-components';
 import { GlobalStyles, theme } from './styles';
 import App from './App';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './util/apollo';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <App />
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
