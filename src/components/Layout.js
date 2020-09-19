47c56e3ef927b279f3cb613f7087ece22c22e1fe
ahmedfaaid <ahmedfaaid.coding@gmail.com>
2020-09-19 19:00:48 -0400

    add appropriate favicons

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -14,28 +14,6 @@ function Layout({ children }) {
     <>
       <Helmet>
         <title>Real Estate</title>
-        {/* <link
-          href='https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap'
-          rel='stylesheet'
-        />
-        <link
-          rel='apple-touch-icon'
-          sizes='180x180'
-          href='/apple-touch-icon.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='32x32'
-          href='/favicon-32x32.png'
-        />
-        <link
-          rel='icon'
-          type='image/png'
-          sizes='16x16'
-          href='/favicon-16x16.png'
-        />
-        <link rel='manifest' href='/site.webmanifest' /> */}
       </Helmet>
       <Header />
       <StyledMain>{children}</StyledMain>
